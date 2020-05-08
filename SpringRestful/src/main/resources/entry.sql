450f119d8658677e3f89292e078a32cd6c365947
Surendra Singh <surendras1312@gmail.com>
2020-05-08 11:52:29 +0530

    sql

--- SpringRestful/src/main/resources/entry.sql
+++ SpringRestful/src/main/resources/entry.sql
@@ -1,3 +1,3 @@
 INSERT INTO evs.evs_tbl_user_credentials
-(userid, password, usertype, loginstatus, login_status, session_id, sessionid)
-VALUES(100, '123', '1', 0, 0, '', '');
+(userid, password, usertype, loginstatus, login_status, session_id)
+VALUES(100, '123', '1', 0, 0, '');
